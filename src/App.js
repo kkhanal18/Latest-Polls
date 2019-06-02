901c3e9f5c7872304b604874b811cadde41b99db
kaushal <0kaushal0@gmail.com>
2019-06-02 02:56:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import PollList from './Components/PollList';
 function App() {
   return (
     <div className="App">
-      <h1>Latest Polls</h1>
+      <p>
+        <h1>Latest Polls</h1><div style={{ opacity: '.3' }}>Last updated 6 hours ago</div>
+      </p>
       <div>
         <PollList />
       </div>
