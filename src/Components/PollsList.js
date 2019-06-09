4275dc55f9142743847a0a29b240888edf64238b
kaushal <0kaushal0@gmail.com>
2019-06-09 11:37:47 -0400

    Update PollsList.js

--- src/Components/PollsList.js
+++ src/Components/PollsList.js
@@ -1,49 +1,32 @@
-import React, { Component } from 'react'
-import { Consumer } from '../context'
+import React, { Component } from "react";
+import { Consumer } from "../context";
 
-
-import PollRow from './PollRow'
-import PollInfo from './PollPage'
+import PollRow from "./PollRow";
+import PollInfo from "./PollPage";
 
 class PollList extends Component {
-    render() {
-        return (
-
-            <Consumer>
-
-
-                {value => {
-                    const { polls } = value
-                    if (polls === undefined || polls.length === 0) {
-
-                        return (
-                            <div class="spinner-border" role="status">
-                                <span class="sr-only">Loading...</span>
-                            </div>)
-
-
-                    } else {
-                        return (
-                            polls.map(poll => (
-                                <React.Fragment>
-
-
-                                    <PollRow key={poll.id} poll={poll} />
-
-                                </React.Fragment>
-
-                            ))
-
-                        )
-
-                    }
-                }}
-
-            </Consumer>
-
-        )
-    }
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { polls } = value;
+          if (polls === undefined || polls.length === 0) {
+            return (
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+            );
+          } else {
+            return polls.map(poll => (
+              <React.Fragment>
+                <PollRow key={poll.id} poll={poll} />
+              </React.Fragment>
+            ));
+          }
+        }}
+      </Consumer>
+    );
+  }
 }
-
 
 export default PollList;
