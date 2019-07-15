6cb9b48c592b8db723bc6b44b8cb5d9b3de0ab31
kaushal <0kaushal0@gmail.com>
2019-07-15 00:02:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-// import PollList from './Components/PollList';
-// import Polls from "./Components/PollsList";
 import PollPage from "./Components/PollPage";
 import Navbar from "./Components/Navbar";
-// import ButtonBar from "./Components/ButtonBar";
 import PollHome from "./Containers/PollHome";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-// import { Provider } from './context'
 
 function App() {
   return (
@@ -17,7 +12,6 @@ function App() {
 
       <div className="container">
         <p style={{ opacity: ".3" }}>Last updated 6 hours ago</p>
-        {/* <ButtonBar /> */}
 
         <Switch>
           <Route exact path="/" component={PollHome} />
