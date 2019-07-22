b4f223cdfb0a9d4dedbcd58fb6b43742d9f39978
kaushal <0kaushal0@gmail.com>
2019-07-21 23:01:25 -0400

    Update PollHome.js

--- src/Containers/PollHome.js
+++ src/Containers/PollHome.js
@@ -3,7 +3,7 @@ import PollList from "../Components/PollsList";
 import ButtonBar from "../Components/ButtonBar";
 import Search from "../Components/Search";
 
-// Poll h
+// Poll hom
 
 const PollHome = () => {
   return (
